33b34aabe75f2604f9467914db6ca9bdfbe278d4
José Pereira Macedo <arseniumgx@gmail.com>
2023-11-10 19:56:46 -0300

    feat: implementação das rotas principais

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,12 @@
-import { Fragment } from 'react';
+import { Route, Routes } from 'react-router-dom';
 
 export function Router() {
-  return <Fragment></Fragment>;
+  return (
+    <Routes>
+      <Route index element={<>HOME</>}></Route>
+      <Route path="locations" element={<>LOCATIONS</>}></Route>
+      <Route path="episodes" element={<>EPISODES</>}></Route>
+      <Route path="*" element={<>NOTFOUND</>} />
+    </Routes>
+  );
 }
